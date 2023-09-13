e4447a68b98b5c62645d18e1c92f8c1b960e23ae
Marcos Bermejo <marcosbermejom@gmail.com>
2023-09-13 12:27:11 +0200

    fixed PM2_SERVE_HOMEPAGE

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
         PM2_SERVE_PATH: './build',
         PM2_SERVE_PORT: 3000,
         PM2_SERVE_SPA: 'true',
-        PM2_SERVE_HOMEPAGE: './index.html'
+        PM2_SERVE_HOMEPAGE: '/index.html'
       }
     }
   ],
