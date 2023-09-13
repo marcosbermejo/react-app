c2a15678e10661ffc905f5784ddf700c542c30cc
Marcos Bermejo <marcosbermejom@gmail.com>
2023-09-13 11:53:30 +0200

    added pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name      : 'my_react_app',
+      script    : 'npm',
+      args      : 'run start:production',
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {},
+    staging: {
+      user: 'ubuntu',
+      host: '3.249.19.249',
+      ref: 'origin/main',
+      repo: 'git@github.com:marcosbermejo/react-app.git',
+      path: '/var/www/html/fcn',
+      key: '/home/marcos/ana.pem',
+      ssh_options: ['ForwardAgent=yes'],
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {}
+  }
+};
